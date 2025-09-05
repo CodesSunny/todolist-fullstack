9b7ef494f4f8e1259d7c52fdf5285db560ac0794
TechSunny <sunnymotivation2@gmail.com>
2025-09-05 13:28:13 +0530

    defined port

--- backend/express.js
+++ backend/express.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 server.use(cors());   //allow cross plateform 
 const fs = require('fs');
