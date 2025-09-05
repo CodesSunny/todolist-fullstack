460c0c0c64850445799a11964b8fa540eeb3e615
TechSunny <sunnymotivation2@gmail.com>
2025-09-05 10:12:50 +0530

    hosting app

---
+++ backend/express.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const server = express();
+const port = 3000;
+const cors = require('cors');
+server.use(cors());   //allow cross plateform 
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './data.json');
+server.use(express.json());   //parse raw json into js object
+
+let todosData = [];    //initialise array to hold tasks, this refers to ram memory not actual db
+
+//read db on get request
+server.get('/', (req,res)=>{
+    const dbData = fs.readFileSync(filePath, 'utf8');     //string
+    todosData = JSON.parse(dbData);         //js object    
+    res.json(todosData);        //send response
+})
+
+
+//append data into db on post request
+server.post('/', (req,res)=>{
+    try {
+        // 1 read/access db
+    let dbData = fs.readFileSync(filePath, 'utf8');     //string
+    todosData = JSON.parse(dbData);         //js object 
+
+    //set id and get task name from req body
+    const clientData = { id: Date.now(), task: req.body.newTask.trim()};   
+    if(!clientData.task || typeof(clientData.task) !== 'string' || clientData.task == ""){
+        return res.status(400).json({message: "bad request"});
+    }
+
+    //2 append new data to local array in memory
+    todosData.push(clientData);
+
+    //3 update array to db
+    fs.writeFileSync(filePath, JSON.stringify(todosData), 'utf8');
+
+    //4 send response new db data 
+    res.json(todosData);
+
+    } catch (error) {
+        console.log(error );
+        res.status(500).json({message: error.message || 'internal server error'})
+    }
+    
+})
+
+
+//delete task on delete request
+server.delete('/:id', (req, res)=>{
+
+    // Extract the task ID from req.params.id
+    const taskId = Number(req.params.id);   //string to number
+
+    // Read the current data from the JSON file
+    let dbData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    //seperate all data except deleted one
+    dbData = dbData.filter(data => data.id !== taskId); 
+    
+    //update memory array
+    todosData = dbData;    
+
+    // update db
+    fs.writeFileSync(filePath, JSON.stringify(todosData), 'utf8');
+    
+    //send response with updated db data 
+    res.json(todosData);
+    
+})
+
+
+//edit task on patch request
+server.patch('/:id', (req,res)=>{
+    try {
+         // 1read existing db data
+        let dbData = fs.readFileSync(filePath, 'utf8');     //string
+        todosData = JSON.parse(dbData);        
+
+        //2. find same task in array with id and update without changing id
+        const editId = Number(req.params.id);
+        const editIndex = todosData.findIndex(item => item.id === editId)  //get index of editing task
+
+        const clientData = {id : editId, task : req.body.editedTask.trim()}; 
+        if(!clientData || clientData.task === "" || typeof(clientData.task) !== "string"){
+           return res.status(400).json({"message" : "bad request"});
+        }
+
+        todosData[editIndex] = clientData;   //update array 
+
+        
+        //4. write back array data to db
+        fs.writeFileSync(filePath, JSON.stringify(todosData),'utf8');
+
+        //5. send response with updated db data
+        res.json( todosData);
+        
+    } catch (err ) {
+        console.log("error occured: ", err  );
+        res.status(500).json({message: "internal server error"});
+    }
+
+})
+
+
+
+
+
+server.listen(port, ()=> console.log(`application listening on port no ${port}`)
+);
---
+++ frontend/index.js
@@ -0,0 +1,141 @@
+const form = document.querySelector("form");
+const todoInput = document.getElementById('todo-input');
+const tbody= document.querySelector('tbody');
+const taskBtn = document.getElementById("task-btn");
+let storedTask = [];  //initialise empty list
+let isEdit = false;    //control edit mode
+let editId = null;    //track task being edited
+
+
+// fetch db and display task on get method
+const displayTask=async ()=>{
+    try{
+        const response = await fetch('http://localhost:3000');
+        
+        if(!response.ok) {
+            throw new Error("response not ok" + response.status)
+        } else{
+            const data =await response.json();
+            storedTask = data   // update local array with DB data             
+            let trTemplate = ""; 
+            
+            //show task from array
+            storedTask.forEach((item, index)=>{   
+                             
+                    trTemplate += `
+                                    <tr>
+                                        <td>${index+1 }</td>
+                                        <td >${item.task }</td>
+                                        <td>
+                                            <i class="fa-solid fa-pen-to-square" onclick="editTask(${item.id}, event)"></i>
+                                            <i  class="fa-solid fa-trash" onclick="deleteTask(${item.id})"></i>
+                                        </td>
+                                    </tr>
+                                `
+                        })     
+            tbody.innerHTML = trTemplate;            
+            return storedTask;
+              }           
+
+    }catch(err){
+        console.log("error fetching data: ", err.message );  
+        alert("some problem in data retreival: ", err.message);
+    }    
+}
+
+displayTask();  //initially display on page load
+
+
+// add new data and send to db
+const saveTask=async()=>{
+   let inputValue = todoInput.value.trim();
+
+   if(!inputValue){
+    alert("write a task");
+    return;
+   }
+
+   try {
+        // edit task : while edit mode 
+   if(isEdit){
+    //change task in input field and send to server
+        const response = await fetch (`http://localhost:3000/${editId}`, {
+            method: 'PATCH',
+            headers:{'Content-Type' : 'application/json'},
+            body: JSON.stringify({editedTask :inputValue})
+        })
+
+    if(!response.ok ){
+        throw new Error("error in fetching patch response ");
+    }
+
+     const data = await response.json();
+
+   }else{
+    // add task : while not edit mode
+    //1 check duplicate task before adding
+    for(let item of storedTask){
+        if(item.task.toLowerCase() === inputValue.toLowerCase()) {
+            alert("task already present");
+            return;
+        } 
+   }
+
+   //2 if not duplicate add new task to db
+    const response = await fetch ('http://localhost:3000', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+                                newTask : inputValue
+                            })
+        })
+        if(!response.ok ){
+        throw new Error("error in fetching post response ");
+    }
+        const data = await response.json();
+    }
+    
+   } catch (err) {
+    console.log( err.message );
+    
+   }
+   
+   await displayTask();
+   todoInput.value = "";
+}
+
+
+// delete task 
+const deleteTask =async (taskId)=>{
+    const response = await fetch (`http://localhost:3000/${taskId}`, {
+        method: 'DELETE'
+    })
+
+    const data = await response.json();   //remaining data after deletion 
+    await displayTask();    
+}
+
+
+//check edit
+const checkEdit =()=>{
+    //set btn text as per edit state
+    taskBtn.textContent = isEdit? "Save Changes" : "Add Task";
+}
+
+
+//edit task
+const editTask=(taskId, e )=>{
+         isEdit = true;
+         editId = taskId;
+         checkEdit();
+
+    //pre fill task in input field
+    todoInput.value = e.target.parentElement.previousElementSibling.textContent.toLowerCase().trim(); 
+}
+ 
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    saveTask();  
+})
